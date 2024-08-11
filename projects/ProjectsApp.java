545f2f4f8a8474f5716c4fc3430cb429d274e795
ABathingDave <david.ramirez12199@gmail.com>
2024-08-10 21:18:34 -0500

    first commit

--- projects/ProjectsApp.java
+++ projects/ProjectsApp.java
@@ -14,10 +14,13 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	// @formatter:off
 	private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project"
 	);
 	// @formatter:on
 
@@ -38,6 +41,14 @@ public class ProjectsApp {
 					
 				case 1:
 					createProject();
+					break;
+					
+				case 2:
+					listProjects();
+					break;
+					
+				case 3:
+					selectProject();
 					break;
 					
 				default:
@@ -123,5 +134,29 @@ public class ProjectsApp {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
 		
 		operations.forEach(line -> System.out.println(" " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
+	}
+	
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+	}
+	
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println("  " + project.getProjectId() + ": " + project.getProjectName()));
 	}
 }
--- projects/service/ProjectService.java
+++ projects/service/ProjectService.java
@@ -9,4 +9,12 @@ public class ProjectService {
 	public Project addProject(Project project) {
 		return projectDao.insertProject(project);
 	}
+	
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects()
+	}
+	
+	public Project fetchProjectbyId(Integer projectId) {
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist."));
+	}
 }
